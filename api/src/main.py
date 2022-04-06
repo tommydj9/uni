39a56f57dbb34bc40627ce35c3cd30cc0b83b96d
tommydj9 <tommasozucca6@gmail.com>
2022-04-06 18:23:55 +0200

    servere and client

--- api/src/main.py
+++ api/src/main.py
@@ -1,13 +1,26 @@
 import flask
+from flask_cors import CORS,cross_origin
+
 
 app = flask.Flask(__name__)
+cors = CORS(app)
 app.config["DEBUG"] = True
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
+
+
+
+
+# Quando arriva una chiama GET all'indirizzo di questo server, seguito da /universities
+# Allora, chiama la funzione getUniversities
+# Poi, il valore che restituisce, lo converti in JSON e lo mandi come risposta via HTTP
 @app.route('/universities', methods=['GET'])
+@cross_origin()
 def getUniversities():
     return {
         "data": ["Politecnico di Milano", "Statale di milano"]
     }
+
 
 app.run()
--- web/src/index.js
+++ web/src/index.js
@@ -1,1 +1,15 @@
-alert("Ciao!")
+
+const serverAddress = 'http://127.0.0.1:5000' // 127.0.0.1 è in HTTP l'indirizzo locale
+// Cioè localhost
+// Cioè, il tuo computer
+// Ci va bene solo finché siamo in fase di sviluppo e lavoriamo sul nostro computer.
+
+axios.get(serverAddress + "/universities")
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
