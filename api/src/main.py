a8e8ddec4810b5fc27ec44a927b4da4ac6eff77b
tommydj9 <tommasozucca6@gmail.com>
2022-04-20 18:15:56 +0200

    add univeriaties

--- api/src/main.py
+++ api/src/main.py
@@ -1,5 +1,5 @@
 import flask
-from flask_cors import CORS,cross_origin
+from flask_cors import CORS, cross_origin
 
 
 app = flask.Flask(__name__)
@@ -8,18 +8,37 @@ app.config["DEBUG"] = True
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 
-
-
-
-
 # Quando arriva una chiama GET all'indirizzo di questo server, seguito da /universities
 # Allora, chiama la funzione getUniversities
 # Poi, il valore che restituisce, lo converti in JSON e lo mandi come risposta via HTTP
 @app.route('/universities', methods=['GET'])
 @cross_origin()
 def getUniversities():
+    # Noi qua leggeremo i dati da una API esterna -> HTTP GET
+    # Quell'API ti restituirà una lista di oggetti/dizionari.
+    # Per ognuno di quelli, noi ne creiamo uno nel formato che vogliamo noi, cioè
+    # Poi li trasformiamo per restituire al front-end dei dati fatti così:
+    #   {
+    #       name: ...
+    #       description: ...
+    #       imgSrc: ...
+    #   }
     return {
-        "data": ["Politecnico di Milano", "Statale di milano"]
+        "universities": [{
+            "name": "Politecnico di Milano",
+            "description": "L'università politecnica più grande d'Italia.",
+            "imgSrc": "https://upload.wikimedia.org/wikipedia/it/b/be/Logo_Politecnico_Milano.png"
+        }, {
+
+            "name": "Statale Milano",
+            "description": "sdfghjklaf.",
+            "imgSrc": "https://upload.wikimedia.org/wikipedia/it/thumb/0/04/Logo_Universit%C3%A0_degli_Studi_di_Milano.svg/1200px-Logo_Universit%C3%A0_degli_Studi_di_Milano.svg.png"
+        },
+            {
+            "name": "Statale Milano",
+            "description": "sdfghjklaf.",
+            "imgSrc": "https://upload.wikimedia.org/wikipedia/it/thumb/0/04/Logo_Universit%C3%A0_degli_Studi_di_Milano.svg/1200px-Logo_Universit%C3%A0_degli_Studi_di_Milano.svg.png"
+        }]
     }
 
 
--- web/src/index.js
+++ web/src/index.js
@@ -5,82 +5,33 @@ const serverAddress = 'http://127.0.0.1:5000' // 127.0.0.1 è in HTTP l'indirizz
 // Cioè, il tuo computer
 // Ci va bene solo finché siamo in fase di sviluppo e lavoriamo sul nostro computer.
 
-const data = response.data
-
 axios.get(serverAddress + "/universities")
-  .then(function (response) {
-    // handle success
-    console.log(response);
+    .then(function (response) {
+        // handle success
 
-    document.getElementById("universities-list").innerHTML = "ohh ispndi"  // Qui avrai in mano quella div...
-    // Poi però che ci fai?
-    // Per ogni università, devi aggiungere qualcosa a quella div
-    // Creare dei children
-    //i dati delle università sono dentro response.data
+        const listOfUniversities = response.data.universities;
 
+        for (let i = 0; i < listOfUniversities.length; i++) {
+            const university = listOfUniversities[i]
+            const universityNode = document.createElement('div');
+            const universityTitleNode = document.createElement('h2');
+            const universityImageNode = document.createElement('img');
+            const universityDescriptionNode = document.createElement('p');
+            universityTitleNode.innerText = university.name;
+            universityImageNode.src = university.imgSrc;
+            universityDescriptionNode.innerText = university.description;
+            universityNode.classList.add('university');
+            universityNode.appendChild(universityTitleNode);
+            universityNode.appendChild(universityImageNode);
+            universityNode.appendChild(universityDescriptionNode);
 
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-
-
-// getting all required elements
-const searchWrapper = document.querySelector(".search-input");
-const inputBox = searchWrapper.querySelector("input");
-const suggBox = searchWrapper.querySelector(".autocom-box");
-const icon = searchWrapper.querySelector(".icon");
-let linkTag = searchWrapper.querySelector("a");
-let webLink;
-// if user press any key and release
-inputBox.onkeyup = (e)=>{
-    let userData = e.target.value; //user enetered data
-    let emptyArray = [];
-    if(userData){
-        icon.onclick = ()=>{
-            webLink = `https://www.google.com/search?q=${userData}`;
-            linkTag.setAttribute("href", webLink);
-            linkTag.click();
+            document.getElementById("universities").appendChild(universityNode);
         }
-        emptyArray = suggestions.filter((data)=>{
-            //filtering array value and user characters to lowercase and return only those words which are start with user enetered chars
-            return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
-        });
-        emptyArray = emptyArray.map((data)=>{
-            // passing return data inside li tag
-            return data = `<li>${data}</li>`;
-        });
-        searchWrapper.classList.add("active"); //show autocomplete box
-        showSuggestions(emptyArray);
-        let allList = suggBox.querySelectorAll("li");
-        for (let i = 0; i < allList.length; i++) {
-            //adding onclick attribute in all li tag
-            allList[i].setAttribute("onclick", "select(this)");
-        }
-    }else{
-        searchWrapper.classList.remove("active"); //hide autocomplete box
-    }
-}
-function select(element){
-    let selectData = element.textContent;
-    inputBox.value = selectData;
-    icon.onclick = ()=>{
-        webLink = `https://www.google.com/search?q=${selectData}`;
-        linkTag.setAttribute("href", webLink);
-        linkTag.click();
-    }
-    searchWrapper.classList.remove("active");
-}
-function showSuggestions(list){
-    let listData;
-    if(!list.length){
-        userValue = inputBox.value;
-        listData = `<li>${userValue}</li>`;
-    }else{
-      listData = list.join('');
-    }
-    suggBox.innerHTML = listData;
-}
 
 
+
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
